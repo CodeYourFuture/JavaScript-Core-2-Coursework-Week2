c203f7e64fdcbe650af97da3e8a83890a16cccd2
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-03-21 11:10:17 +0200

    Completed E-list-of-colours in re-cap class

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,23 +1,58 @@
-function listOfColours(colourArr) {
-    let content = document.getElementById("content");
+// function listOfColours(colourArr) {
+//     let content = document.getElementById("content");
+//     let selectEl = document.createElement("select");
+//      content.appendChild(selectEl);
+//      let pEl = document.createElement("p");
+//      content.appendChild(pEl);
+//   for (let colour of colourArr){
+//       selectedColour = function (){
+//       pEl.textContent = "You have selected: " + colourArr[selectEl.selectedIndex];
+//       pEl.style.color = colourArr[selectEl.selectedIndex];
+//       }
+//     pEl.textContent = "You have selected: " + selectEl.selectedIndex;
+//     let optionEl = document.createElement("option");
+//     selectEl.appendChild(optionEl);
+//     optionEl.innerHTML = colour;
+//     console.log(colour);
+//     selectEl.addEventListener("click", selectedColour);
+//   //  console.log(selectEl.selectedIndex);
+//   }
+// }
+
+ 
+
+
+
+function listOfColours(colours){
+   const targetDiv = document.getElementById("content");
+   console.log("This is my targetDiv",targetDiv);
+
     let selectEl = document.createElement("select");
-     content.appendChild(selectEl);
-     let pEl = document.createElement("p");
-     content.appendChild(pEl);
-  for (let colour of colourArr){
-      selectedColour = function (){
-      pEl.textContent = "You have selected: " + colourArr[selectEl.selectedIndex];
-      pEl.style.color = colourArr[selectEl.selectedIndex];
-      }
-    pEl.textContent = "You have selected: " + selectEl.selectedIndex;
-    let optionEl = document.createElement("option");
-    selectEl.appendChild(optionEl);
-    optionEl.innerHTML = colour;
-    console.log(colour);
-    selectEl.addEventListener("click", selectedColour);
-  //  console.log(selectEl.selectedIndex);
-  }
-}
+    targetDiv.appendChild(selectEl);
+    //parent.appendChild(child)
+    let pEl = document.createElement("p");
+      targetDiv.appendChild(pEl);
+      pEl.textContent = "You have selected: "
+
+    colours.forEach(colourName => {
+      // console.log("This is my colour in the array",colourName);
+
+      let colourDrop = document.createElement("option");
+      colourDrop.textContent = colourName;
+      selectEl.appendChild(colourDrop);
+
+      selectColour = function(){
+      pEl.style.color = selectEl.value;
+      pEl.textContent = "You have selected: " + selectEl.value;
+    }
+    });
+    //Complete final step in the click event to add the name of the 
+    //selected color to the p element textContent.
+    
+    selectEl.addEventListener("click", selectColour);
+    
+ }
+
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
 
