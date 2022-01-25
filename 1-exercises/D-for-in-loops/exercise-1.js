34aa7a340be676bd2063ed5fc4a15db3798af882
Joshua Dysiewicz <joshuadysiewicz@googlemail.com>
2022-01-25 22:31:41 +0000

    todo add for in exercises

---
+++ 1-exercises/D-for-in-loops/exercise-1.js
@@ -0,0 +1,21 @@
+/*
+
+   Use a "for...in" loop to log out the trainee name (in capital letters)
+   and their grade, but only if they scored 19 or more.
+
+*/
+
+let traineeGrades = {
+  tom: 20,
+  george: 17,
+  abdul: 19,
+};
+
+// WRITE BELOW THIS LINE
+
+
+
+
+// Expected Result:
+// TOM: 20
+// ABDUL: 19
