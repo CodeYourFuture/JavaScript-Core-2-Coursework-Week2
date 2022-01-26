d89fb579665f98b1112f0ce060b462383a268803
JDysieiwcz <joshuadysiewicz@googlemail.com>
2022-01-26 08:53:46 +0000

    added object array exercises

---
+++ 1-exercises/B-object-arrays/exercise-1.js
@@ -0,0 +1,19 @@
+/*
+
+   The "people" array contains objects which describe a person.
+   Create an array containing only the names of all the people in the array.
+
+   hint: use the array "map" method.
+
+*/
+
+let people = [
+   {name: "Bob", age: 30},
+   {name: "John", age: 20},
+   {name: "Elise", age: 35}
+]
+
+let peopleNames = /* todo */
+
+// DO NOT EDIT BELOW THIS LINE
+console.log(`Expected output: ["Bob", "John", "Elise"]. Actual output: ${peopleNames}`)
---
+++ 1-exercises/B-object-arrays/exercise-2.js
@@ -0,0 +1,23 @@
+/*
+
+   The "store" array contains various items you can buy, along with their price.
+   Create an array containing the names of all the things you could buy (less than
+   the "money" variable").
+
+   hint: use the array "filter" method.
+
+*/
+
+let store = [
+   {item: "Corn Flakes", price: 2.50},
+   {item: "Milk", price: 1.20},
+   {item: "Chocolate", price: 0.80}
+]
+
+let money = 1.50;
+
+let thingsICanBuy = /* todo */
+
+// DO NOT EDIT BELOW THIS LINE
+console.log(`Expected output: ["Milk", "Chocolate"]`. Actual output: ${thingsICanBuy})
+
