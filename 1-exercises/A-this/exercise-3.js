b365050d7c705faec218888a66eed4f5acf32eca
DeagoBrowne <89912823+DeagoBrowne@users.noreply.github.com>
2022-01-30 20:14:32 +0000

    A - complete

--- 1-exercises/A-this/exercise-3.js
+++ 1-exercises/A-this/exercise-3.js
@@ -8,8 +8,14 @@
 
 let bankAccount = {
   currentAmount: 100,
-  deposit: function (amount) {},
-  withdraw: function (amount) {},
+  deposit: function (amount) {
+    this.currentAmount += amount;
+    return this.currentAmount;
+  },
+  withdraw: function (amount) {
+    this.currentAmount -= amount;
+    return this.currentAmount;
+  },
 };
 
 // DO NOT EDIT BELOW THIS LINE
