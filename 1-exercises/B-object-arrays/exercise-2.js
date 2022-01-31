a1377997e0f41a8bb1da1610bcde5fe0a88cac16
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-01-31 17:34:39 +0000

    Add B-object-arrays 1 and 2.

--- 1-exercises/B-object-arrays/exercise-2.js
+++ 1-exercises/B-object-arrays/exercise-2.js
@@ -16,10 +16,9 @@ let store = [
 
 let money = 1.5;
 
-let thingsICanBuy =
-  /* todo */
+let thingsICanBuy = store
+  .filter((product) => product.price < money)
+  .map((product) => product.item);
 
-  // DO NOT EDIT BELOW THIS LINE
-  console.log(
-    `Expected output: Milk,Chocolate. Actual output: ${thingsICanBuy}`
-  );
+// DO NOT EDIT BELOW THIS LINE
+console.log(`Expected output: Milk,Chocolate. Actual output: ${thingsICanBuy}`);
