63b443f002b2130ad6b6f98451b2373a3f954bee
mahri09 <63245237+mahri09@users.noreply.github.com>
2022-02-02 09:57:55 +0000

    completed  exercizes

--- 1-exercises/C-object-methods/exercise-3.js
+++ 1-exercises/C-object-methods/exercise-3.js
@@ -12,11 +12,10 @@ let highScores = {
   100: ["David"],
 };
 
-let highScoreKeys = /* todo */
-let highScoreValues = /* todo */
-
-  // DO NOT EDIT BELOW THIS LINE
-  console.log(`Expected keys: 34, 55, 89, 100, Actual keys: ${highScoreKeys}`);
+let highScoreKeys = Object.keys(highScores);
+let highScoreValues = Object.values(highScores);
+// DO NOT EDIT BELOW THIS LINE
+console.log(`Expected keys: 34, 55, 89, 100, Actual keys: ${highScoreKeys}`);
 console.log(
   `Expected values: Sergi, Frank, Alistair, Mary, Hannah, David. Actual values: ${highScoreValues}`
 );
