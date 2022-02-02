4ffbcb15274f5ab1a46dd9c38517151836b660fc
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-02-02 16:24:40 +0000

    Add 2-mandatory, 6-budgets

--- 2-mandatory/6-budgets.js
+++ 2-mandatory/6-budgets.js
@@ -11,7 +11,11 @@ Should give return the answer of 62600.
 **/
 
 function getBudgets(peopleArray) {
+  return peopleArray.reduce((sum, person) => {
+    return (sum += person.budget);
+  }, 0);
 }
+
 
 /* ======= TESTS - DO MODIFY (!!!) =====
 - To run the tests for this exercise, run `npm test -- --testPathPattern 9-budgets.js`
