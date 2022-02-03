a5d522609a8bc0b3bb34fac8efaafc28d7d297f1
Margarita Glushkova <m.glushkova1@gmail.com>
2022-02-03 16:42:17 +0000

    5-9 exercises complete
    Mandatory folder

--- 2-mandatory/6-budgets.js
+++ 2-mandatory/6-budgets.js
@@ -11,8 +11,14 @@ Should give return the answer of 62600.
 **/
 
 function getBudgets(peopleArray) {
+  let totalBudget = 0;
+for(let person of peopleArray) {
+  totalBudget += person.budget
+}
+return totalBudget;
 }
 
+// expected output: 10
 /* ======= TESTS - DO MODIFY (!!!) =====
 - To run the tests for this exercise, run `npm test -- --testPathPattern 9-budgets.js`
 - To run all exercises/tests in the mandatory folder, run `npm test`
