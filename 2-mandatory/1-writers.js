8562b85a7179446ba99a7dd950271b4970b76414
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-02-04 19:39:28 +0000

    Update 1-writers.js

--- 2-mandatory/1-writers.js
+++ 2-mandatory/1-writers.js
@@ -60,8 +60,12 @@ Exercise 1:
   "Hi, my name is {firstName} {lastName}. I am {age} years old, and work as a {occupation}."
 */
 function logAllWriters() {
-  // write your code to log all writers here
-};
+  for (writer of writers) {
+    console.log(
+      `Hi, my name is ${writer.firstName} ${writer.lastName}. I am ${writer.age} years old, and work as a ${writer.occupation}.`
+    );
+  }
+}
 
 /*
 Exercise 2:
@@ -71,7 +75,13 @@ Exercise 2:
 */
 
 function logDeadWritersInTheirForties() {
-  // write your code here
+  for (writer of writers) {
+    if (writer.age >= 40 && writer.age < 50 && writer.alive === false) {
+      console.log(
+        `Writer ${writer.firstName} ${writer.lastName} died at ${writer.age} years old.`
+      );
+    }
+  }
 }
 
 /*
@@ -81,8 +91,15 @@ Exercise 3:
 */
 
 function logAliveWritersInTheirForties() {
-  // write your code here
+  for (writer of writers) {
+    if (writer.age >= 40 && writer.age < 50 && writer.alive === true) {
+      console.log(
+        `Hi, my name is ${writer.firstName} ${writer.lastName}. I am ${writer.age} years old.`
+      );
+    }
+  }
 }
+
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern 1-writers.js`
