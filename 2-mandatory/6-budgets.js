7573c65c6cc45f7e8460381bf5108cef8e24f416
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-02-04 19:44:09 +0000

    Update 6-budgets.js

--- 2-mandatory/6-budgets.js
+++ 2-mandatory/6-budgets.js
@@ -11,6 +11,11 @@ Should give return the answer of 62600.
 **/
 
 function getBudgets(peopleArray) {
+  let totalBudget = 0;
+  for (let person of peopleArray) {
+    totalBudget += person.budget;
+  }
+  return totalBudget;
 }
 
 /* ======= TESTS - DO MODIFY (!!!) =====
