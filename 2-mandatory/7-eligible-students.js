e54ed667fc36f6e1141023e1b22fb8014a90fa15
Ika Khoza <ikaneng.khoza@gmail.com>
2022-02-10 11:02:30 +0000

    modified budget exercise
    modified budget exercise

--- 2-mandatory/7-eligible-students.js
+++ 2-mandatory/7-eligible-students.js
@@ -14,7 +14,14 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
  */
 
-function eligibleStudents(attendances) {}
+function eligibleStudents(attendances) {
+
+
+
+} 
+
+
+
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern 2-eligible-students.js`
--- 2-mandatory/8-journey-planner.js
+++ 2-mandatory/8-journey-planner.js
@@ -19,7 +19,9 @@
   What's better about each approach?
 */
 
-function journeyPlanner(locations, transportMode) {}
+function journeyPlanner(locations, transportMode) {
+  
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern 3-journey-planner.js`
