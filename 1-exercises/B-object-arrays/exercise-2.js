acb8d130c2047c91b37a5c9a572a6c88e2131060
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-09-19 09:46:35 +0100

    Update exercise-2.js

--- 1-exercises/B-object-arrays/exercise-2.js
+++ 1-exercises/B-object-arrays/exercise-2.js
@@ -1,25 +1,20 @@
 /*
-
    The "store" array contains various items you can buy, along with their price.
    Create an array containing the names of all the things you could buy (less than
    the "money" variable").
-
    hint: use the array "filter" method and "map" method in a chain.
-
 */
 
-let store = [
-  { item: "Corn Flakes", price: 2.5 },
-  { item: "Milk", price: 1.2 },
-  { item: "Chocolate", price: 0.8 },
+let store =
+[
+    { item: "Corn Flakes", price: 2.5 },
+    { item: "Milk", price: 1.2 },
+    { item: "Chocolate", price: 0.8 },
 ];
 
 let money = 1.5;
 
-let thingsICanBuy =
-  /* todo */
+let thingsICanBuy = store.filter(store => store.price < money).map(store => store.item);
 
-  // DO NOT EDIT BELOW THIS LINE
-  console.log(
-    `Expected output: Milk,Chocolate. Actual output: ${thingsICanBuy}`
-  );
+// DO NOT EDIT BELOW THIS LINE
+console.log(`Expected output: Milk,Chocolate. Actual output: ${thingsICanBuy}`);
