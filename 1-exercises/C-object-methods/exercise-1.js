dcb72254b1628ed2907cab00d876cbe8bfe5e1d5
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-09-19 09:51:02 +0100

    Update exercise-1.js

--- 1-exercises/C-object-methods/exercise-1.js
+++ 1-exercises/C-object-methods/exercise-1.js
@@ -1,20 +1,16 @@
 /*
-
    Use the appropriate "Object." static method to list the keys in the "footballClubs" object.
    Assign this to the "clubNames" variable.
-
 */
 
-let footballClubs = {
-  chelsea_fc: "England",
-  fc_barcelona: "Spain",
-  ac_milan: "Italy",
+let footballClubs =
+{
+    chelsea_fc: "England",
+    fc_barcelona: "Spain",
+    ac_milan: "Italy",
 };
+  
+let clubNames = Object.keys(footballClubs);
 
-let clubNames =
-  /* todo */
-
-  // DO NOT EDIT BELOW THIS LINE
-  console.log(
-    `Expected value: chelsea_fc, fc_barcelona, ac_milan. Actual value: ${clubNames}`
-  );
+// DO NOT EDIT BELOW THIS LINE
+console.log(`Expected value: chelsea_fc, fc_barcelona, ac_milan. Actual value: ${clubNames}`);
