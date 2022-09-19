b6b80b98c9faa5a1a78690f48562dd877437daad
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-09-19 10:09:12 +0100

    Update exercise-1.js

--- 1-exercises/D-for-in-loops/exercise-1.js
+++ 1-exercises/D-for-in-loops/exercise-1.js
@@ -1,16 +1,23 @@
 /*
-
    Use a "for...in" loop to log out the name of
    each person who earns more than 50000. The
    log should include a "$" symbol before each salary.
-
 */
 
-let salaries = {
-  ross: 30000,
-  monica: 55000,
-  rachel: 90000,
+let salaries =
+{
+    ross: 30000,
+    monica: 55000,
+    rachel: 90000,
 };
+
+for (const item in salaries)
+{
+    if(salaries[item] > 50000)
+    {
+        console.log("$" + salaries[item]);
+    }
+}
 
 // WRITE BELOW THIS LINE
 
