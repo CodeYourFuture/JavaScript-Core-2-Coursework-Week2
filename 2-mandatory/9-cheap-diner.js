1e1364c2a1385e30c57468d64c278e82a401da3e
SaadiaElf <saadiaelfekak@gmail.com>
2022-09-19 23:19:51 +0100

    Solution ex9

--- 2-mandatory/9-cheap-diner.js
+++ 2-mandatory/9-cheap-diner.js
@@ -19,7 +19,17 @@ chosenMeal(emptyArray)
 Should give the answer "Nothing :("
 **/
 
-function chooseMeal(mealArray) {}
+function chooseMeal(mealArray) {
+  mealArray.sort((a, b) => a.price - b.price);
+
+  if (mealArray.length == 0) {
+    return "Nothing :(";
+  } else if (mealArray.length == 1) {
+    return `${mealArray[0].name}`;
+  } else {
+    return `${mealArray[1].name}`;
+  }
+}
 
 /* ======= TESTS - DO MODIFY (!!!) =====
 - To run the tests for this exercise, run `npm test -- --testPathPattern 10-cheap-diner.js`
