ed32981c038c0e6aeb2e558679ef945ed7340189
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2022-09-19 09:33:15 +0100

    Update exercise-3.js

--- 1-exercises/A-this/exercise-3.js
+++ 1-exercises/A-this/exercise-3.js
@@ -6,19 +6,22 @@
 
 */
 
-let bankAccount = {
-  currentAmount: 100,
-  deposit: function (amount) {},
-  withdraw: function (amount) {},
+let bankAccount =
+{
+    currentAmount: 100,
+    deposit: function (amount)
+    {
+        return this.currentAmount += amount;
+    },
+    withdraw: function (amount)
+    {
+        return this.currentAmount -= amount;
+    },
 };
-
+  
 // DO NOT EDIT BELOW THIS LINE
 bankAccount.deposit(50);
-console.log(
-  `Expected Result: 150. Actual result: ${bankAccount.currentAmount}`
-);
+console.log(`Expected Result: 150. Actual result: ${bankAccount.currentAmount}`);
 
 bankAccount.withdraw(25);
-console.log(
-  `Expected Result: 125. Actual result: ${bankAccount.currentAmount}`
-);
+console.log(`Expected Result: 125. Actual result: ${bankAccount.currentAmount}`);
