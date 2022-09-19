7609327e291b311b4f823a2e09e4400f7b2c23db
JadeJones2022 <100792140+JadeJones2022@users.noreply.github.com>
2022-09-19 22:08:30 +0100

    journey

--- 2-mandatory/8-journey-planner.js
+++ 2-mandatory/8-journey-planner.js
@@ -19,7 +19,15 @@
   What's better about each approach?
 */
 
-function journeyPlanner(locations, transportMode) {}
+function journeyPlanner(locations, transportMode) {
+  let suitableTransport = []
+  
+  for (let location in locations)
+  if (locations[location].includes(transportMode)){
+   suitableTransport.push(location); 
+  }
+  return suitableTransport
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern 3-journey-planner.js`
