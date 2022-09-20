c423f04cb3b5a75d8571b5c39a8733d3f49793ea
Kawa MANMI <kawamam2007@gmail.com>
2022-09-20 22:42:20 +0100

    3-Mandatory  has been done

--- 2-mandatory/3-groceries.js
+++ 2-mandatory/3-groceries.js
@@ -27,14 +27,27 @@ Exercise 1:
 */
 // Gather all week item names into this array
 let weeklyGroceriesToBuy = [];
-
+for (const day in weeklyMealPlan) {
+  weeklyMealPlan[day].map((dailyGroceries) => {
+    if (weeklyGroceriesToBuy.indexOf(dailyGroceries) === -1)
+      weeklyGroceriesToBuy.push(dailyGroceries);
+  });
+}
 /*
 Exercise 2:
   Loop through your list again, but now only collect the weekend items into the weekendGroceriesToBuy array.
 */
 // Gather weekend item names into this array
 let weekendGroceriesToBuy = [];
-
+for (const day in weeklyMealPlan) {
+  weeklyMealPlan[day].map((dailyGroceries) => {
+    if (
+      weekendGroceriesToBuy.indexOf(dailyGroceries) === -1 &&
+      day === "saturday"
+    )
+      weekendGroceriesToBuy.push(dailyGroceries);
+  });
+}
 /*
 Exercise 3:
   Loop through your weekly meal plan:
@@ -51,6 +64,9 @@ let numberOfItemsPerWeek = {
   saturday: 0,
   sunday: 0,
 };
+for (const day in weeklyMealPlan) {
+  numberOfItemsPerWeek[day] = weeklyMealPlan[day].length;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern 5-groceries.js`
@@ -60,26 +76,48 @@ let numberOfItemsPerWeek = {
 
 test("Exercise 1 - Weekly groceries to buy contains correct items", () => {
   const expectedWeeklyGroceriesToBuy = [
-    'Cheese',       'Eggs',
-    'Tomato',       'Paprika',
-    'Leek',         'Wrap',
-    'Tuna',         'Canned beans',
-    'Carrot',       'Aubergine',
-    'Orange Juice', 'Apple',
-    'Ananas',       'Black tea',
-    'Lamb',         'Salt',
-    'Bulgur',       'Potato',
-    'Rice milk',    'Blueberries',
-    'Porridge',     'Banana',
-    'Cinnamon',     'Olive oil',
-    'Salmon',       'Asparagus'
+    "Cheese",
+    "Eggs",
+    "Tomato",
+    "Paprika",
+    "Leek",
+    "Wrap",
+    "Tuna",
+    "Canned beans",
+    "Carrot",
+    "Aubergine",
+    "Orange Juice",
+    "Apple",
+    "Ananas",
+    "Black tea",
+    "Lamb",
+    "Salt",
+    "Bulgur",
+    "Potato",
+    "Rice milk",
+    "Blueberries",
+    "Porridge",
+    "Banana",
+    "Cinnamon",
+    "Olive oil",
+    "Salmon",
+    "Asparagus",
   ];
-  expect(weeklyGroceriesToBuy).toIncludeSameMembers(expectedWeeklyGroceriesToBuy);
+  expect(weeklyGroceriesToBuy).toIncludeSameMembers(
+    expectedWeeklyGroceriesToBuy
+  );
 });
 
 test("Exercise 2 - Weekend groceries to buy contains correct items", () => {
-  const expectedWeekendGroceriesToBuy = ["Olive oil", "Potato", "Salmon", "Asparagus"];
-  expect(weekendGroceriesToBuy).toIncludeSameMembers(expectedWeekendGroceriesToBuy);
+  const expectedWeekendGroceriesToBuy = [
+    "Olive oil",
+    "Potato",
+    "Salmon",
+    "Asparagus",
+  ];
+  expect(weekendGroceriesToBuy).toIncludeSameMembers(
+    expectedWeekendGroceriesToBuy
+  );
 });
 
 test("Exercise 3 - Numer of items per week contains the correct counts", () => {
--- 2-mandatory/kawatest.js
+++ 2-mandatory/kawatest.js
@@ -1,49 +1,18 @@
-let writers = [
-  {
-    firstName: "Virginia",
-    lastName: "Woolf",
-    occupation: "writer",
-    age: 59,
-    alive: false,
-  },
-  {
-    firstName: "Zadie",
-    lastName: "Smith",
-    occupation: "writer",
-    age: 40,
-    alive: true,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Austen",
-    occupation: "writer",
-    age: 41,
-    alive: false,
-  },
-  {
-    firstName: "Bell",
-    lastName: "Hooks",
-    occupation: "writer",
-    age: 63,
-    alive: true,
-  },
-  {
-    firstName: "Yukiko",
-    lastName: "Motoya",
-    occupation: "writer",
-    age: 49,
-    alive: true,
-  },
-];
-
-function logAliveWritersInTheirForties() {
-  // write your code here
-  writers.forEach((write) => {
-    if (write["age"] >= 40 && write["age"] <= 49 && write["alive"] === true)
-      console.log(
-        `Hi, my name is ${write["firstName"]} ${write["lastName"]}. I am  ${write["age"]} years old.`
-      );
+let weeklyMealPlan = {
+  monday: ["Cheese", "Eggs", "Tomato", "Paprika", "Leek"],
+  tuesday: ["Wrap", "Tuna", "Canned beans", "Cheese", "Carrot", "Aubergine"],
+  wednesday: ["Orange Juice", "Apple", "Ananas", "Black tea"],
+  thursday: ["Lamb", "Salt", "Bulgur", "Potato"],
+  friday: ["Rice milk", "Blueberries", "Porridge", "Banana", "Cinnamon"],
+  saturday: ["Olive oil", "Potato", "Salmon", "Asparagus"],
+  sunday: [],
+};
+let weeklyGroceriesToBuy = [];
+for (const day in weeklyMealPlan) {
+  weeklyMealPlan[day].map((dailyGroceries) => {
+    if (weeklyGroceriesToBuy.indexOf(dailyGroceries) === -1)
+      weeklyGroceriesToBuy.push(dailyGroceries);
   });
 }
 
-logAliveWritersInTheirForties();
+console.log(weeklyGroceriesToBuy);
