cf460d99c048f17b428c38fcb3a20f4a775b07c8
Lunamzelo <lunamzelo@gmail.com>
2022-09-23 18:36:53 +0100

    finished exercise-D 1

--- 1-exercises/D-for-in-loops/exercise-1.js
+++ 1-exercises/D-for-in-loops/exercise-1.js
@@ -14,6 +14,14 @@ let salaries = {
 
 // WRITE BELOW THIS LINE
 
+for (const name in salaries) {
+  if (salaries[name] >50000){
+     console.log(`$${[name]}: $${salaries[name]}`)
+
+
+  }
+}
+
 // Expected Result:
 // monica: $55000
 // rachel: $90000
