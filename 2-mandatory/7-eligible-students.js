6b23c77d1ed5c331339bb828e7635061c8afdcb6
Lunamzelo <lunamzelo@gmail.com>
2022-09-23 22:52:50 +0100

    finished mandatory-7

--- 2-mandatory/7-eligible-students.js
+++ 2-mandatory/7-eligible-students.js
@@ -14,7 +14,15 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
  */
 
-function eligibleStudents(attendances) {}
+function eligibleStudents(attendances) {
+  let isAttended = [];
+  for (const key of attendances) {
+    if (key.attendance >= 8) {
+      isAttended.push(key.name);
+    }
+  }
+  return isAttended;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern 2-eligible-students.js`
