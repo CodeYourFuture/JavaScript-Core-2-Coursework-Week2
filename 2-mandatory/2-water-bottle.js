00f062a19683693d9afada788391e4a63f11c6fc
micheale2021 <andemariammicheale@gmail.com>
2022-09-23 12:41:35 +0100

    commplete mandatory/2-water-bottle.js

--- 2-mandatory/2-water-bottle.js
+++ 2-mandatory/2-water-bottle.js
@@ -19,18 +19,27 @@ let bottle = {
   volume: 0,
   fillUp: function () {
     // calling this function should completely fill your bottle (volume = 100);
+    return this.volume = 100;
   },
   pour: function () {
     // calling this function should increase your bottle volume by 10 units;
+    return this.volume = this.volume + 10;
   },
   drink: function () {
     // calling this function should decrease your bottle volume by 10 units;
+    return this.volume = this.volume - 10;
   },
   isFull: function () {
     // this function should return true if your bottle is full;
+    if (this.volume === 100) {
+     return true;
+    }
   },
   isEmpty: function () {
     // this function should return true if your bottle is empty;
+    if (this.volume === 0) {
+      return true;
+     }
   },
 };
 
