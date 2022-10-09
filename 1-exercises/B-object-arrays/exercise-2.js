67a38eedf39a3475bc1e20507d5f2ba67cfad5e0
Anosh Malik <anoshmalik.am@gmail.com>
2022-10-09 19:05:32 +0100

    Unfinished 1B exercise 2

--- 1-exercises/B-object-arrays/exercise-2.js
+++ 1-exercises/B-object-arrays/exercise-2.js
@@ -16,10 +16,8 @@ let store = [
 
 let money = 1.5;
 
-let thingsICanBuy =
-  /* todo */
+let thingsICanBuy = store.map((moneys) => moneys.price).filter((prices) => prices<money);
+/* todo */
 
-  // DO NOT EDIT BELOW THIS LINE
-  console.log(
-    `Expected output: Milk,Chocolate. Actual output: ${thingsICanBuy}`
-  );
+// DO NOT EDIT BELOW THIS LINE
+console.log(`Expected output: Milk,Chocolate. Actual output: ${thingsICanBuy}`);
