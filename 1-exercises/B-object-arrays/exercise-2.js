87bea54940bf8d79aacce4823e3df7f52de77a48
Zaw Khing <zawmyokhing@gmail.com>
2022-10-09 23:03:06 +0100

    Complete exercise A&B

--- 1-exercises/B-object-arrays/exercise-2.js
+++ 1-exercises/B-object-arrays/exercise-2.js
@@ -16,7 +16,7 @@ let store = [
 
 let money = 1.5;
 
-let thingsICanBuy =
+let thingsICanBuy = store.filter((el)=> el.price < money).map(el => el.item)
   /* todo */
 
   // DO NOT EDIT BELOW THIS LINE
