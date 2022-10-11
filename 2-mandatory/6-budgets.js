f8b7d973d9de600375144c8b1a99a84e94247bcf
baharbazargan62 <baharbazargan62@gmail.com>
2022-10-11 12:38:49 +0100

    mandatory 6-7

--- 2-mandatory/6-budgets.js
+++ 2-mandatory/6-budgets.js
@@ -10,8 +10,16 @@ getBudgets([
 Should give return the answer of 62600.
 **/
 
-function getBudgets(peopleArray) {
-}
+  function getBudgets(peopleArray) {
+    let sum=0
+    if (peopleArray===[]){return 0
+    }else 
+    for (let key of peopleArray){
+      sum+=key.budget
+    }
+    return sum;
+  }
+
 
 /* ======= TESTS - DO MODIFY (!!!) =====
 - To run the tests for this exercise, run `npm test -- --testPathPattern 9-budgets.js`
--- 2-mandatory/7-eligible-students.js
+++ 2-mandatory/7-eligible-students.js
@@ -14,7 +14,15 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
  */
 
-function eligibleStudents(attendances) {}
+function eligibleStudents(attendances) {
+let array=[]
+  for (let key of attendances){
+    if (key.attendance>=8 ){
+     array.push(key.name)
+    }
+  }
+  return array;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern 2-eligible-students.js`
