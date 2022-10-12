139e9d27c00bc44aa3b2a390e38f22e01eaa0331
Ekrem <ekremteke.uk@gmail.com>
2022-10-13 00:41:10 +0100

    exercises of D were done

--- 1-exercises/D-for-in-loops/exercise-1.js
+++ 1-exercises/D-for-in-loops/exercise-1.js
@@ -11,6 +11,11 @@ let salaries = {
   monica: 55000,
   rachel: 90000,
 };
+for (let name in salaries) {
+  if (salaries[name] > 50000) {
+    console.log(`${name}: $${salaries[name]}`);
+  }
+}
 
 // WRITE BELOW THIS LINE
 
