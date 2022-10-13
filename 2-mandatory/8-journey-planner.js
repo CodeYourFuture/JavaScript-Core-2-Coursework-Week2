871b58860ac354bce9111df11cc6307a9a2aa4c3
Adibab <adiba.du.bd@gmail.com>
2022-10-13 09:45:23 +0100

    complete 8-journey-planner.js

--- 2-mandatory/8-journey-planner.js
+++ 2-mandatory/8-journey-planner.js
@@ -19,7 +19,15 @@
   What's better about each approach?
 */
 
-function journeyPlanner(locations, transportMode) {}
+function journeyPlanner(locations, transportMode) {
+  let filteredLoc = [];
+  for (let exptedlocArray in locations) {
+    if (locations[exptedlocArray].includes(transportMode)) {
+      filteredLoc.push(exptedlocArray);
+    }
+  }
+  return filteredLoc;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern 3-journey-planner.js`
