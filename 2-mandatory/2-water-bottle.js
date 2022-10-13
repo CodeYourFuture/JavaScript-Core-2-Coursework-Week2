85d4ccd1ae25d7f2de0090ad23795d927826cbb8
Ekrem <ekremteke.uk@gmail.com>
2022-10-13 22:47:11 +0100

    mondatory 2 was done

--- 2-mandatory/2-water-bottle.js
+++ 2-mandatory/2-water-bottle.js
@@ -15,22 +15,33 @@ You have to implement the missing features according to the specification.
 */
 
 // Here is your starting point:
+
 let bottle = {
   volume: 0,
   fillUp: function () {
+    let add = 100 - this.volume;
+    return (this.volume += add);
     // calling this function should completely fill your bottle (volume = 100);
   },
   pour: function () {
     // calling this function should increase your bottle volume by 10 units;
+    if (this.volume !== 100) {
+      return (this.volume += 10);
+    }
   },
   drink: function () {
     // calling this function should decrease your bottle volume by 10 units;
+    if (this.volume !== 0) {
+      return (this.volume -= 10);
+    }
   },
   isFull: function () {
     // this function should return true if your bottle is full;
+    return this.volume == 100;
   },
   isEmpty: function () {
     // this function should return true if your bottle is empty;
+    return this.volume == 0;
   },
 };
 
@@ -54,7 +65,7 @@ and see if your answer matches the expected result at the bottom :)
 */
 
 /* ======= TESTS - DO NOT MODIFY =====
-- To run the tests for this exercise, run `npm test -- --testPathPattern 4-water-bottle.js`
+- To run the tests for this exercise, run `npm test -- --testPathPattern 2-water-bottle.js`
 - To run all exercises/tests in the mandatory folder, run `npm test`
 - (Reminder: You must have run `npm install` one time before this will work!)
 */
