d7f888d002a8b872b6c86810c4d775a7caef4ba0
Ekrem <ekremteke.uk@gmail.com>
2022-10-14 17:55:18 +0100

    mandatory seven was done

--- 2-mandatory/7-eligible-students.js
+++ 2-mandatory/7-eligible-students.js
@@ -14,10 +14,18 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
  */
 
-function eligibleStudents(attendances) {}
+function eligibleStudents(attendances) {
+  let nameOfEligibleToSitAnExam = [];
+  for (let student of attendances) {
+    if (student.attendance >= 8) {
+      nameOfEligibleToSitAnExam.push(student.name);
+    }
+  }
+  return nameOfEligibleToSitAnExam;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
-- To run the tests for this exercise, run `npm test -- --testPathPattern 2-eligible-students.js`
+- To run the tests for this exercise, run `npm test -- --testPathPattern 7-eligible-students.js`
 - To run all exercises/tests in the mandatory folder, run `npm test`
 - (Reminder: You must have run `npm install` one time before this will work!)
 */
