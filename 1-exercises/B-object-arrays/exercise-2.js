aebfff0b155937d4d0ed89553313a46a83f3c730
Bahare <itxbb09@gmail.com>
2023-01-09 15:39:54 +0000

    Exercises Done

--- 1-exercises/B-object-arrays/exercise-2.js
+++ 1-exercises/B-object-arrays/exercise-2.js
@@ -16,10 +16,10 @@ let store = [
 
 let money = 1.5;
 
-let thingsICanBuy =
-  /* todo */
+let thingsICanBuy = store
+  .filter((element) => element.price <= money)
+  .map((el) => el.item);
 
-  // DO NOT EDIT BELOW THIS LINE
-  console.log(
-    `Expected output: Milk,Chocolate. Actual output: ${thingsICanBuy}`
-  );
+
+// DO NOT EDIT BELOW THIS LINE
+console.log(`Expected output: Milk,Chocolate. Actual output: ${thingsICanBuy}`);
