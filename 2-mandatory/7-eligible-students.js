57a1febe10b317368ed9e31eeeb8a90d4a94e1f5
ys35code <ys35code@gmail.com>
2023-01-12 20:52:38 +0000

    2-mandatory-7

--- 2-mandatory/7-eligible-students.js
+++ 2-mandatory/7-eligible-students.js
@@ -14,10 +14,18 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
  */
 
-function eligibleStudents(attendances) {}
+function eligibleStudents(attendances) {
+  let arr = [];
+  for (let students of attendances) {
+    if (students.attendance > 7) {
+      arr.push(students.name);
+    }
+  }
+  return arr;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
-- To run the tests for this exercise, run `npm test -- --testPathPattern 2-eligible-students.js`
+- To run the tests for this exercise, run `npm test -- --testPathPattern 7-eligible-students.js`
 - To run all exercises/tests in the mandatory folder, run `npm test`
 - (Reminder: You must have run `npm install` one time before this will work!)
 */
