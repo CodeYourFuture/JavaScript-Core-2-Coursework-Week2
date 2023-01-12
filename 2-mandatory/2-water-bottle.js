0b2ca8a3363c007c0c63b578b85e5feac4c026f3
Zobeir <rigi.zobir@yahoo.com>
2023-01-12 19:03:30 +0000

    mandatory 2

--- 2-mandatory/2-water-bottle.js
+++ 2-mandatory/2-water-bottle.js
@@ -19,18 +19,27 @@ let bottle = {
   volume: 0,
   fillUp: function () {
     // calling this function should completely fill your bottle (volume = 100);
+    this.volume = 100
   },
   pour: function () {
     // calling this function should increase your bottle volume by 10 units;
+   if (this.volume < 100 ) {
+    return this.volume += 10 ;
+   }
   },
   drink: function () {
     // calling this function should decrease your bottle volume by 10 units;
+    if(this.volume > 0){
+      return this.volume -= 10;
+    }
   },
   isFull: function () {
     // this function should return true if your bottle is full;
+    return bottle.volume === 100 ;
   },
   isEmpty: function () {
     // this function should return true if your bottle is empty;
+    return this.volume === 0
   },
 };
 
@@ -46,7 +55,12 @@ Extra question:
   Leave your answer below:
 */
 
-// Write you answer to the question here
+// Write you answer to the question here 
+/* There isn't really any efficiency advantage to using "this" instead of using the object's name. It's mostly for simplicity purposes 
+(although it can admittedly be confusing at first) since "this" can be used to refer to any of the current object's members 
+(members being things like variables or arguments). Even though it may seem pointless, it's generally seen as best practice.
+*/
+
 
 /*
 Once you have completed your object run the following 
