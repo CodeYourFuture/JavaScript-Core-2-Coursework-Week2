0adc05b54bac4bb5b17121b918803a213f2ea6b4
Bahare <itxbb09@gmail.com>
2023-01-13 12:23:38 +0000

    Extra Done

--- 3-extra/1-choose-your-own-adventure.js
+++ 3-extra/1-choose-your-own-adventure.js
@@ -44,6 +44,9 @@ let game = {
     // object for the correct room.
     //
     // Hint: the only valid rooms are "hall", "classroom" and "library".
+    if (Object.keys(rooms).includes(roomName)) {
+      this.currentRoom = rooms[roomName];
+    }
   },
 
   move: function (direction) {
@@ -53,6 +56,12 @@ let game = {
     //
     // Hint: the room objects have north/east/south/west methods which return
     // a new room object that is in the relevant direction.
+    if (
+      Object.keys(rooms.hall).includes(direction) &&
+      this.currentRoom[direction] !== null
+    ) {
+      this.currentRoom = this.currentRoom[direction]();
+    }
   },
 };
 
