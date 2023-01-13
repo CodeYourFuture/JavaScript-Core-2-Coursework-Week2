30db31c2f684eacc99f7441d495a84f899c53fda
LorenaCapraru <108892538+LorenaCapraru@users.noreply.github.com>
2023-01-13 19:04:28 +0000

    Mandatory.7 completed

--- 2-mandatory/7-eligible-students.js
+++ 2-mandatory/7-eligible-students.js
@@ -14,7 +14,15 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
  */
 
-function eligibleStudents(attendances) {}
+function eligibleStudents(attendances) {
+  let eligibleStudents = [];
+  for (let student of attendances) {
+    if (student.attendance >= 8) {
+      eligibleStudents.push(student.name);
+    }
+  }
+  return eligibleStudents;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern 2-eligible-students.js`
