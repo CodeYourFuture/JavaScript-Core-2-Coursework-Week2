d4f82db67bc02d9aae99da3cb69d04192505acca
Olena Reukova <kronverk01@gmail.com>
2023-01-20 23:27:52 +0000

    2-mandatory/1-writers

--- 2-mandatory/1-writers.js
+++ 2-mandatory/1-writers.js
@@ -50,7 +50,7 @@ let writers = [
     occupation: "writer",
     age: 49,
     alive: true,
-  }
+  },
 ];
 
 /*
@@ -61,7 +61,12 @@ Exercise 1:
 */
 function logAllWriters() {
   // write your code to log all writers here
-};
+  writers.forEach((x) =>
+    console.log(
+      `Hi, my name is ${x.firstName} ${x.lastName}. I am ${x.age} years old, and work as a ${x.occupation}.`
+    )
+  );
+}
 
 /*
 Exercise 2:
@@ -72,6 +77,13 @@ Exercise 2:
 
 function logDeadWritersInTheirForties() {
   // write your code here
+  let notAlive = writers.filter((x) => {
+    if (x.age >= 40 && x.age < 50 && !x.alive) {
+      console.log(
+        `Writer ${x.firstName} ${x.lastName} died at ${x.age} years old.`
+      );
+    }
+  });
 }
 
 /*
@@ -82,6 +94,11 @@ Exercise 3:
 
 function logAliveWritersInTheirForties() {
   // write your code here
+  let aliveWriters = writers.filter((x) => {
+    if (x.age > 40 && x.age < 50 && x.alive) {
+      console.log(`Hi, my name is ${x.firstName} ${x.lastName}. I am ${x.age} years old.`)
+    }
+  });
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== 
@@ -90,29 +107,32 @@ function logAliveWritersInTheirForties() {
 - (Reminder: You must have run `npm install` one time before this will work!)
 */
 
-test("exercise 1", () => expectFunctionToLog(logAllWriters, [
-  "Hi, my name is Virginia Woolf. I am 59 years old, and work as a writer.",
-  "Hi, my name is Zadie Smith. I am 40 years old, and work as a writer.",
-  "Hi, my name is Jane Austen. I am 41 years old, and work as a writer.",
-  "Hi, my name is Bell Hooks. I am 63 years old, and work as a writer.",
-  "Hi, my name is Yukiko Motoya. I am 49 years old, and work as a writer."
-]));
+test("exercise 1", () =>
+  expectFunctionToLog(logAllWriters, [
+    "Hi, my name is Virginia Woolf. I am 59 years old, and work as a writer.",
+    "Hi, my name is Zadie Smith. I am 40 years old, and work as a writer.",
+    "Hi, my name is Jane Austen. I am 41 years old, and work as a writer.",
+    "Hi, my name is Bell Hooks. I am 63 years old, and work as a writer.",
+    "Hi, my name is Yukiko Motoya. I am 49 years old, and work as a writer.",
+  ]));
 
-test("exercise 2", () => expectFunctionToLog(logDeadWritersInTheirForties, [
-  "Writer Jane Austen died at 41 years old."
-]));
+test("exercise 2", () =>
+  expectFunctionToLog(logDeadWritersInTheirForties, [
+    "Writer Jane Austen died at 41 years old.",
+  ]));
 
-test("exercise 3", () => expectFunctionToLog(logAliveWritersInTheirForties, [
-  "Hi, my name is Zadie Smith. I am 40 years old.",
-  "Hi, my name is Yukiko Motoya. I am 49 years old."
-]));
+test("exercise 3", () =>
+  expectFunctionToLog(logAliveWritersInTheirForties, [
+    "Hi, my name is Zadie Smith. I am 40 years old.",
+    "Hi, my name is Yukiko Motoya. I am 49 years old.",
+  ]));
 
 function expectFunctionToLog(f, values) {
-    const consoleLogSpy = jest.spyOn(console, 'log');
-    f();
-    expect(consoleLogSpy).toBeCalledTimes(values.length);
-    values.forEach((value, i) => {
-      expect(consoleLogSpy).nthCalledWith(i+1, value);
-    });
-    consoleLogSpy.mockRestore();
-};
+  const consoleLogSpy = jest.spyOn(console, "log");
+  f();
+  expect(consoleLogSpy).toBeCalledTimes(values.length);
+  values.forEach((value, i) => {
+    expect(consoleLogSpy).nthCalledWith(i + 1, value);
+  });
+  consoleLogSpy.mockRestore();
+}
