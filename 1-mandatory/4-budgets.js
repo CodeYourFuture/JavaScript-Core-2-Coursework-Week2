e042bfed113b3a2e0884c50b1544760095d5fae1
BEKIRKSU <113244972+BEKIRKSU@users.noreply.github.com>
2023-03-22 19:00:41 +0000

    M4 done

--- 1-mandatory/4-budgets.js
+++ 1-mandatory/4-budgets.js
@@ -10,7 +10,15 @@ getBudgets([
 Should give return the answer of 62600.
 **/
 
-function getBudgets(peopleArray) {}
+function getBudgets(peopleArray) {
+    let totalBudget = 0;
+  
+    for (let i = 0; i < peopleArray.length; i++) {
+      totalBudget += peopleArray[i].budget;
+    }
+  
+    return totalBudget;
+  }
 
 /* ======= TESTS - DO MODIFY (!!!) =====
 - To run the tests for this exercise, run `npm test -- --testPathPattern budgets.js`
