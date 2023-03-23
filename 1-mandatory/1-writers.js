28dcdb3c3f191a17912849c1f41cb066ae53e197
malkitbenning <malkitbenning@gmail.com>
2023-03-23 15:49:44 +0000

    completed first exercise in mandatory

--- 1-mandatory/1-writers.js
+++ 1-mandatory/1-writers.js
@@ -51,7 +51,11 @@ Exercise 1:
   "Hi, my name is {firstName} {lastName}. I am {age} years old, and work as a {occupation}."
 */
 function logAllWriters() {
-  // write your code to log all writers here
+  writers.map((writer) => {
+    console.log(
+      `Hi, my name is ${writer.firstName} ${writer.lastName}. I am ${writer.age} years old, and work as a ${writer.occupation}.`
+    );
+  });
 }
 
 /*
@@ -62,7 +66,14 @@ Exercise 2:
 */
 
 function logDeadWritersInTheirForties() {
-  // write your code here
+  let deadWritersIn40s = writers.filter((writer) => {
+    return writer.age > 39 && writer.age < 50 && !writer.alive;
+  });
+  deadWritersIn40s.map((writer) => {
+    console.log(
+      `Writer ${writer.firstName} ${writer.lastName} died at ${writer.age} years old.`
+    );
+  });
 }
 
 /*
@@ -72,7 +83,14 @@ Exercise 3:
 */
 
 function logAliveWritersInTheirForties() {
-  // write your code here
+  let aliveWritersIn40s = writers.filter((writer) => {
+    return writer.age > 39 && writer.age < 50 && writer.alive;
+  });
+  aliveWritersIn40s.map((writer) => {
+    console.log(
+      `Hi, my name is ${writer.firstName} ${writer.lastName}. I am ${writer.age} years old.`
+    );
+  });
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== 
