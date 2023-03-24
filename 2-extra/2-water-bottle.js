96176fa0e624eb70fb1bf47d47bdbbf348461314
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-03-24 00:50:32 +0000

    Extra - 2

--- 2-extra/2-water-bottle.js
+++ 2-extra/2-water-bottle.js
@@ -19,18 +19,34 @@ let bottle = {
   volume: 0,
   fillUp: function () {
     // calling this function should completely fill your bottle (volume = 100);
+    this.volume = 100;
+    return this.volume;
   },
   pour: function () {
     // calling this function should increase your bottle volume by 10 units;
+    if (this.volume <= 90) {
+      this.volume += 10;
+    } else {
+      this.volume = 100;
+    }
+    return this.volume;
   },
   drink: function () {
     // calling this function should decrease your bottle volume by 10 units;
+    if (this.volume >= 10) {
+      this.volume -= 10;
+    } else {
+      this.volume = 0;
+    }
+    return this.volume;
   },
   isFull: function () {
     // this function should return true if your bottle is full;
+    return this.volume == 100;
   },
   isEmpty: function () {
     // this function should return true if your bottle is empty;
+    return this.volume == 0;
   },
 };
 
