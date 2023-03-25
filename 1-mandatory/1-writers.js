ad8a7818c81db68acfc7e05be752e3fc395dac60
Danny Romero <108970600+Elenar9@users.noreply.github.com>
2023-03-25 12:13:19 +0000

    mandatory/1

--- 1-mandatory/1-writers.js
+++ 1-mandatory/1-writers.js
@@ -51,7 +51,9 @@ Exercise 1:
   "Hi, my name is {firstName} {lastName}. I am {age} years old, and work as a {occupation}."
 */
 function logAllWriters() {
-  // write your code to log all writers here
+ writers.forEach(writer => console.log(
+  `Hi, my name is ${writer.firstName} ${writer.lastName}. I am ${writer.age} years old, and work as a ${writer.occupation}.`
+  ))
 }
 
 /*
@@ -62,7 +64,11 @@ Exercise 2:
 */
 
 function logDeadWritersInTheirForties() {
-  // write your code here
+  writers.forEach(writer => {
+    if (writer.age >= 40 && writer.age <= 49 && writer.alive == false){
+      console.log(`Writer ${writer.firstName} ${writer.lastName} died at ${writer.age} years old.`)
+    }
+  })
 }
 
 /*
@@ -72,7 +78,11 @@ Exercise 3:
 */
 
 function logAliveWritersInTheirForties() {
-  // write your code here
+  writers.forEach(writer => {
+    if (writer.age >= 40 && writer.age<= 49 && writer.alive == true) {
+      console.log(`Hi, my name is ${writer.firstName} ${writer.lastName}. I am ${writer.age} years old.`)
+    }
+  })
 }
 
 /* ======= TESTS - DO NOT MODIFY ===== 
