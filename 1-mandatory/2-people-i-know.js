16b1564358efb662c2a30f32bf1bbb46f1e2196e
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-03-28 20:17:35 +0100

    completed find friends who work for powernet function
    used .filter() method to find friends who work at Powernet
    used .map() method to add email address from the filtered array to new array

--- 1-mandatory/2-people-i-know.js
+++ 1-mandatory/2-people-i-know.js
@@ -376,9 +376,8 @@ const friends = [
 In the above object you can see my friends and the colleagues of my friends.
 First, I want you to find all of my friends who are 35 or older.
 */
-
-let thirtyFiveOrOlder = [];
-//used .filter() method to return array items with friend.age greater than 35 and then used .map() method on the items in the temp filtered array to add the name of friends greater than 35 to array named thirtyFiveOrOlder  
+//used .filter() method to return array items with friend.age greater than 35 and then used .map() method on the items in the temp filtered array to add the value of friend.name.first to array thirtyFiveOrOlder 
+let thirtyFiveOrOlder = []; 
 function findFriendsOverThirtyFive(friendsList) {
   friendsList.filter((friend) => friend.age >= 35).map((friend) => thirtyFiveOrOlder.push(friend.name.first))
   return thirtyFiveOrOlder;
@@ -387,9 +386,12 @@ function findFriendsOverThirtyFive(friendsList) {
 3) Find the email address
 Next, I want you to find all of my friends who work for "POWERNET" and then store their emails in the array below
 */
-
+//used to .filter() method to return items with friend.company equal to "POWERNET" and then used .map() method on the temp filtered array to add the value of friend.email to array powerNetEmails
 let powerNetEmails = [];
-
+function findFriendsWhoWorkForPOWERNET(friendsList) {
+  friendsList.filter((friend) => friend.company === "POWERNET").map((friend) => powerNetEmails.push(friend.email));
+  return powerNetEmails;
+}
 /*
 4) colleagues with "Stacie Villarreal"
 Next, I want you to find all of my friends who are colleagues of Stacie Villarreal.
