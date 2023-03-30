306bdaba7ec6d8a6943c73a01faaee7fab30480e
nasir ali <a.nasirali@outlook.com>
2023-03-30 14:53:35 +0100

    fixed extra 4

--- 2-extra/4-eligible-students.js
+++ 2-extra/4-eligible-students.js
@@ -14,7 +14,15 @@
   - Returns an array containing only the names of the who have attended AT LEAST 8 classes
  */
 
-function eligibleStudents(attendances) {}
+function eligibleStudents(attendances) {
+  let eligible = [];
+  for (student of attendances) {
+    if (student.attendance >= 8) {
+      eligible.push(student.name);
+    }
+  }
+  return eligible;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 - To run the tests for this exercise, run `npm test -- --testPathPattern eligible-students.js`
